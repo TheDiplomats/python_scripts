c7fad5fa5634198e14f502ce5db9e3a10f6695ba
TheDiplomats <socx@protonmail.com>
2020-05-30 11:38:11 -0700

    Experiencing minor problem with test

--- v2/forensics/ghp/my_test.py
+++ v2/forensics/ghp/my_test.py
@@ -6,4 +6,23 @@ pid = raw_input("Enter the PID of the process to attach to: ")
 
 debugger.attach(int(pid))
 
+t_list = debugger.enumerate_threads()
+
+# For each thread in the list grab the value of each register #
+
+for thread in t_list:
+	
+	thread_context = debugger.get_thread_context(thread)
+	
+	print("[*] Dumping the contents of some registers")
+	print("[+] Thread 0x%08x" % thread)
+	print("[+] EIP: 0x%08x" % thread_context.Eip)
+	print("[+] ESP: 0x%08x" % thread_context.Esp)
+	print("[+] EBP: 0x%08x" % thread_context.Ebp)
+	print("[+] EAX: 0x%08x" % thread_context.Eax)
+	print("[+] EBX: 0x%08x" % thread_context.Ebx)
+	print("[+] ECX: 0x%08x" % thread_context.Ecx)
+	print("[+] EDX: 0x%08x" % thread_context.Edx)
+	print("[*] End of dump")
+	
 debugger.detach()
